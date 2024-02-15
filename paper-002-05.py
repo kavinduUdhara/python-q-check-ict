075bc0182c6b0fc9e9e8ee2a4e751fc4740b07c7
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-02-15 17:33:25 +0530

    finalized paper-002

--- paper-002-05.py
+++ paper-002-05.py
@@ -9,17 +9,3 @@ def find_equal(arr1, arr2):
                 return i
 
 find_equal(lst1, lst2)
-
-lst1 = [5, 8, 7, 3, 2]
-lst2 = [4, 2, 6, 8, 7]
-
-def find_equal(arr1, arr2):
-    z = 0
-    for i in arr1:
-        for x in arr2:
-            z += 1
-            print(z)
-            if i == x:
-                return i
-
-find_equal(lst1, lst2)
