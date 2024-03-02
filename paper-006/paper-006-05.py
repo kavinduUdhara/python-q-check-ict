5b67cc0266a9299985eccf947da288abf99b2d56
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-03-02 12:03:39 +0530

    renamed paper-006-05 to paper-006-06 and added new file paper-006-05

---
+++ paper-006/paper-006-05.py
@@ -0,0 +1,10 @@
+def cal_bonus(salary):
+    return salary * 0.5
+def cal_total_sal(salary = 50000, yos = 1):
+    if yos > 5:
+        bonus = cal_bonus(salary)
+        print(f"salary after bonus: {salary + bonus}")
+    else:
+        print(f"salary without bonus: {salary}")
+        
+cal_total_sal(yos=10, salary=70000)
