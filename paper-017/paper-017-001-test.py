eca5ebe3f64323f0145bf0a9d686e80e6fd7f630
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-03-19 22:53:39 +0530

    commit bofore sign off

--- paper-017/paper-017-001-test.py
+++ paper-017/paper-017-001-test.py
@@ -2,9 +2,7 @@ total = 0
 count = 0
 marks = [10, -20, 60, 50, 100, 101, 60]
 for mark in marks:
-    if mark < 0:
-        continue
-    elif mark > 100:
+    if mark > 100:
         break
     else:
         count += 1
